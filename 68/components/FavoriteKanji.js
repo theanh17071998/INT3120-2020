545b7dbf2b46b7998367ec339e2ee06b6acb3dd3
17021084 <dotrunghg1999@gmail.com>
2020-05-29 09:20:55 +0700

    thêm state và sửa cấu trúc của context api trong file FavoriteKanji

--- 68/components/FavoriteKanji.js
+++ 68/components/FavoriteKanji.js
@@ -1,122 +1,166 @@
-import React from 'react';
+import React, { useState } from "react";
 import {
-  Text,
-  View,
-  StyleSheet,
-  TextInput,
-  Dimensions,
-  Button
-} from 'react-native';
+	Text,
+	View,
+	StyleSheet,
+	TextInput,
+	Dimensions,
+	Button,
+	TouchableOpacity,
+} from "react-native";
 
-const deviceWidth = Dimensions.get('window').width;
+import { Context } from "../screens/FavoriteKanjiScreen/context";
+
+const deviceWidth = Dimensions.get("window").width;
+const deviceHeight = Dimensions.get("window").height;
+const heightScreen = (percent) => (percent * deviceHeight) / 100;
 const screen = (percent) => (deviceWidth * percent) / 100;
-export default function FavoriteKanji() {
-  return (
-    <View>
-      <Text style={styles.title}>
-        Tạo Kanji của riêng bạn
-      </Text>
-      <View style={styles.contentInput}>
-        <Text style={styles.titleInput}>
-          Hán tự
-        </Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Nhập hán tự của bạn"
-        />
-      </View>
-      <View style={styles.contentInput}>
-        <Text style={styles.titleInput}>
-          Nghĩa Hán-Việt
-        </Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Nhập nghĩa hán việt"
-        />
-      </View>
-      <View style={styles.contentInput}>
-        <Text style={styles.titleInput}>
-          Âm on
-        </Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Nhập âm on"
-        />
-      </View>
-      <View style={styles.contentInput}>
-        <Text style={styles.titleInput}>
-          Âm kun
-        </Text>
 
-        <TextInput
-          style={styles.textInput}
-          placeholder="Nhập âm kun"
-        />
-      </View>
-      <View style={styles.contentInput}>
-        <Text style={styles.titleInput}>
-          Ví dụ cách sử dụng
-        </Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Ví dụ"
-        />
-      </View>
-      <View style={styles.button}>
-        <Button
-          title="Lưu lại"
-          color="#006265"
-          raised
-        />
-      </View>
-    </View>
-  );
+function FavoriteKanji(props) {
+	const [kanji, setKanji] = useState("");
+	const [hanViet, setHanViet] = useState("");
+	const [amOn, setAmOn] = useState("");
+	const [amKun, setAmKun] = useState("");
+	const [example, setExample] = useState("");
+
+	return (
+		<View style={styles.modal}>
+			<Text style={styles.title}>Tạo kanji của riêng bạn</Text>
+			<View style={styles.contentInput}>
+				<Text style={styles.titleInput}>Hán tự</Text>
+				<TextInput
+					style={styles.textInput}
+					value={kanji}
+					onChangeText={(value) => setKanji(value)}
+					placeholder="Nhập hán tự của bạn"
+				/>
+			</View>
+			<View style={styles.contentInput}>
+				<Text style={styles.titleInput}>Nghĩa Hán-Việt</Text>
+				<TextInput
+					style={styles.textInput}
+					value={hanViet}
+					onChangeText={(value) => setHanViet(value)}
+					placeholder="Nhập nghĩa hán việt"
+				/>
+			</View>
+			<View style={styles.contentInput}>
+				<Text style={styles.titleInput}>Âm on</Text>
+				<TextInput
+					style={styles.textInput}
+					value={amOn}
+					onChangeText={(value) => setAmOn(value)}
+					placeholder="Nhập âm on"
+				/>
+			</View>
+			<View style={styles.contentInput}>
+				<Text style={styles.titleInput}>Âm kun</Text>
+
+				<TextInput
+					style={styles.textInput}
+					value={amKun}
+					onChangeText={(value) => setAmKun(value)}
+					placeholder="Nhập âm kun"
+				/>
+			</View>
+			<View style={styles.contentInput}>
+				<Text style={styles.titleInput}>Ví dụ cách sử dụng</Text>
+				<TextInput
+					multiline
+					numberOfLines={4}
+					value={example}
+					onChangeText={(value) => setExample(value)}
+					style={styles.textInput}
+					placeholder="Ví dụ"
+				/>
+			</View>
+			<View style={{ marginTop: screen(1) }}>
+				<TouchableOpacity style={styles.button}>
+					<Button
+						title="Lưu lại"
+						color="#006265"
+						raised
+						onPress={() => {
+							props.setStateModal(!props.stateModal);
+						}}
+					/>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button}>
+					<Button
+						title="Hủy"
+						color="#fd6363"
+						raised
+						onPress={() => {
+							props.setStateModal(!props.stateModal);
+						}}
+					/>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
 }
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    padding: 16,
-    elevation: 4,
-    borderRadius: 5,
-    backgroundColor: 'white',
-    marginBottom: 11,
-    borderWidth: 0,
-    flexDirection: 'row',
-
-  },
-  title: {
-    fontSize: 19,
-    color: '#006265',
-    textAlign: 'center',
-    fontWeight: 'bold',
-    marginTop: screen(3),
-    marginBottom: screen(5)
-  },
-  ImageChallenge: {
-    width: 30,
-    height: 30,
-    marginLeft: '2%'
-  },
-  contentInput: {
-    marginBottom: screen(3)
-  },
-  textInput: {
-    width: screen(94),
-    borderColor: 'gray',
-    borderWidth: 1,
-    height: screen(12),
-    marginLeft: screen(3),
-    borderRadius: 5,
-    padding: 10,
-  },
-  titleInput: {
-    marginLeft: screen(3),
-    fontSize: 17,
-    marginBottom: 3,
-  },
-  button: {
-    marginTop: screen(5),
-    width: screen(94),
-    marginLeft: screen(3),
-  }
+	modal: {
+		height: heightScreen(80),
+		width: screen(100),
+	},
+	container: {
+		alignItems: "center",
+		padding: 16,
+		elevation: 4,
+		borderRadius: 5,
+		backgroundColor: "white",
+		marginBottom: 11,
+		borderWidth: 0,
+		flexDirection: "row",
+	},
+	title: {
+		backgroundColor: "#006265",
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
+		height: heightScreen(7),
+		margin: 20,
+		fontSize: 19,
+		color: "#fff",
+		textAlign: "center",
+		fontWeight: "bold",
+		marginTop: 0,
+		paddingTop: heightScreen(1),
+		marginBottom: screen(2),
+	},
+	ImageChallenge: {
+		width: 30,
+		height: 30,
+		marginLeft: "2%",
+	},
+	contentInput: {
+		marginHorizontal: 40,
+		color: "#006265",
+		marginBottom: screen(3),
+	},
+	textInput: {
+		borderColor: "#006265",
+		borderWidth: 1,
+		borderRadius: 5,
+		padding: 10,
+	},
+	titleInput: {
+		color: "#006265",
+		fontSize: 17,
+		marginBottom: 3,
+	},
+	button: {
+		marginTop: screen(2),
+		width: screen(80),
+		marginLeft: screen(10),
+	},
 });
+export default function FavoriteKanjiContainer() {
+	return (
+		<Context.Consumer>
+			{({ setStateModal, stateModal }) => (
+				<FavoriteKanji setStateModal={setStateModal} stateModal={stateModal} />
+			)}
+		</Context.Consumer>
+	);
+}
